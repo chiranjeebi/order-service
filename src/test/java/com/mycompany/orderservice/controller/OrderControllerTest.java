6037d84620868504b9d439dd06ec1e51c4c3e6d8
chiranjeebi <mallikchiranjeebi@gmail.com>
2022-06-15 14:16:05 +0530

    commit order control test

--- src/test/java/com/mycompany/orderservice/controller/OrderControllerTest.java
+++ src/test/java/com/mycompany/orderservice/controller/OrderControllerTest.java
@@ -2,6 +2,8 @@ package com.mycompany.orderservice.controller;
 
 import com.mycompany.orderservice.dto.BookDTO;
 import com.mycompany.orderservice.dto.OrderDTO;
+import com.mycompany.orderservice.entity.OrderEntity;
+import com.mycompany.orderservice.repository.OrderRepository;
 import com.mycompany.orderservice.service.OrderService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -16,6 +18,7 @@ import org.springframework.http.ResponseEntity;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -25,8 +28,11 @@ public class OrderControllerTest {
 
     @InjectMocks
     private OrderController orderController;
+    @Mock
+    private OrderRepository orderRepository;
 @Mock
     private OrderService orderService;
+
 
     @Test
     @DisplayName("scenario for placing order")
@@ -42,7 +48,7 @@ Mockito.when(orderService.placeOrder(Mockito.any())).thenReturn(orderDTO);
 
     }
 
-/*    @Test
+    @Test
     @DisplayName("scenario for get all order")
     void testGetAllOrders(){
 
@@ -51,14 +57,17 @@ Mockito.when(orderService.placeOrder(Mockito.any())).thenReturn(orderDTO);
      orderDTO.setUserId(2L);
      orderDTO.setId(3L);
         orderDTOList.add(orderDTO);
+    Optional<OrderEntity> order = orderRepository.findById(3L);
 
         Mockito.when(orderService.getAllOrders(Mockito.any())).thenReturn(orderDTOList);
-        ResponseEntity<List<OrderDTO>> responseEntity =  orderController.getAllOrders();
-        Assertions.assertNotNull(responseEntity.getBody().size());
-        Assertions.assertEquals(HttpStatus.OK.value(),responseEntity.getStatusCodeValue());
+  ResponseEntity<OrderDTO> responseEntity=orderController.getAllOrders(orderDTOList,2L);
+           assertEquals(2l,responseEntity.getBody().getUserId());
+        assertEquals(HttpStatus.CREATED.value(), responseEntity.getStatusCodeValue());
 
 
 
-    }*/
+    }
+
+
 
 }
